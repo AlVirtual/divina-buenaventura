9dedfd6e3a32490be7ec734d1fd1b63ac3601dbb
AlVirtual <virtualweb2020@gmail.com>
2023-02-14 19:55:43 +0100

    deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: /divina-buenaventura/
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/divina-buenaventura/'
+    : '/'
 })
 
 
